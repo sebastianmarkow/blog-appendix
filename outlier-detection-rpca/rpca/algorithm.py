adb08d2d8b2afb21b260462fffca1de9ab21d2a0
Sebastian Klatt <sebastian@markow.io>
2018-06-28 22:13:36 +0200

    Stage

--- outlier-detection-rpca/rpca/algorithm.py
+++ outlier-detection-rpca/rpca/algorithm.py
@@ -1,4 +1,6 @@
-""""""
+"""
+Robust Principal Component Analysis
+"""
 
 import numpy as np
 
@@ -7,9 +9,65 @@ from numpy.linalg import svd
 
 
 def rpca(M, mu=None, l=None, tol=1E-7, max_iter=1000):
-    """"""
+    """Matrix recovery/decomposition using Robust Principal Component Analysis
+    with Augmented Lagrangian Method (ALM)
+
+    Decompose a rectengular matrix M into a low-rank component, and a sparse
+    component, by solving a convex program called Principal Component Pursuit.
+
+    minimize        ||A||_* + λ ||E||_1
+    subject to      A + E = M
+
+    where           ||A||_* is the nuclear norm of A (sum of singular values)
+                    ||E||_1 is the l1 norm of E (absolute values of elements)
+
+    Parameters
+    ----------
+
+    M : array-like, shape (n_samples, n_features)
+        Matrix to decompose, where n_samples in the number of samples and n_features
+        is the number of features.
+
+    mu : float (default 1.25 * ||M||_2)
+        Parameter from the Augmented Lagrange Multiplier form of Principal
+        Component Pursuit (PCP). [2]_
+
+    l : float (default 1/sqrt(max(m,n)), for m x n of M)
+        Parameter of the convex problem ||A||_* + l ||E||_1. [2]_
+
+    tol : float >= 0 (default 1E-7)
+        Tolerance for accuracy of matrix reconstruction of low rank and sparse
+        components.
+
+    max_iter : int >= 0 (default 1000)
+        Maximum number of iterations to perform.
+
+
+    Returns
+    -------
+
+    A : array, shape (n_samples, n_features)
+        Low-rank component of the matrix decomposition.
+
+    E : array, shape (n_samples, n_features)
+        Sparse component of the matrix decomposition.
+
+    err : float
+        Error of matrix reconstruction
+
+    References
+    ----------
+
+    .. [1] Z. Lin, M. Chen, Y. Ma. The Augmented Lagrange Multiplier Method for
+           Exact Recovery of Corrupted Low-Rank Matrices, arXiv:1009.5055
+
+    .. [2] E. J. Candés, X. Li, Y. Ma, J. Wright. Robust principal
+           component analysis? Journal of the ACM v.58 n.11 May 2011
+
+    """
+
     if not mu:
-        mu = norm(M, ord=2, axis=(0,1)) * 1.25
+        mu = 1.25 * norm(M, ord=2, axis=(0,1))
 
     if not l:
         l = 1 / np.sqrt(np.max(M.shape))
@@ -18,33 +76,33 @@ def rpca(M, mu=None, l=None, tol=1E-7, max_iter=1000):
     A = np.zeros(M.shape)
     E = np.zeros(M.shape)
 
-    M_sign = np.sign(M)
-    M_sign_norm2 = norm(M_sign, ord=2)
-    M_sign_normi = norm(M_sign, ord=np.inf) * (l**-1)
+    M_sgn = np.sign(M)
+    M_spectral = norm(M_sgn, ord=2)
+    M_absolute = norm(M_sgn, ord=np.inf) * (l**-1)
 
-    J = np.max([M_sign_norm2, M_sign_normi])
-    Y = M_sign / J
+    Y = M_sgn / np.max([M_spectral, M_absolute])
 
     err = np.inf
     i = 0
 
     while err > tol and i < max_iter:
         U, S, V = svd(M - E + Y * (mu**-1), full_matrices=False)
+
         A = np.dot(U, np.dot(np.diag(_shrink(S, mu)), V))
         E = _shrink(M - A + Y * (mu**-1), l * mu)
         Y = Y + mu * (M - A - E)
 
-        err = _calc_error(M, A, E)
+        err = _fro_error(M, A, E)
         i += 1
 
-    return A, E
+    return A, E, err
 
 
-def _calc_error(M, A, E):
-    """"""
+def _fro_error(M, A, E):
+    """Error of matrix reconstruction"""
     return norm(M - A - E, ord='fro') / norm(M, ord='fro')
 
 
 def _shrink(M, t):
-    """"""
+    """Shrinkage operator"""
     return np.sign(M) * np.maximum((np.abs(M) - t), np.zeros(M.shape))
--- outlier-detection-rpca/rpca/transformer.py
+++ outlier-detection-rpca/rpca/transformer.py
@@ -1,4 +1,6 @@
-""""""
+"""
+Robust Principal Component Analysis Scikit-Learn transformer
+"""
 
 from sklearn.base import TransformerMixin
 
@@ -6,25 +8,102 @@ from algorithm import rpca
 
 
 class RobustPCA(TransformerMixin):
+    """Robust Principal Component Analysis with Augmented Lagrangian Method
+
+    Decompose a rectengular matrix M into a low-rank component, and a sparse
+    component, by solving a convex program called Principal Component Pursuit.
+
+    minimize        ||A||_* + λ ||E||_1
+    subject to      A + E = M
+
+    where           ||A||_* is the nuclear norm of A (sum of singular values)
+                    ||E||_1 is the l1 norm of E (absolute values of elements)
+
+    Parameters
+    ----------
+
+    method : string {sparse, low_rank}
+        sparse: Transformation will yield the sparse component
+        low_rank: Transformation will yield the low-rank component
+
+    mu : float (default 1.25 * ||M||_2)
+        Parameter from the Augmented Lagrange Multiplier form of Principal
+        Component Pursuit (PCP). [2]_
+
+    l : float (default 1/sqrt(max(m,n)), for m x n of M)
+        Parameter of the convex problem ||A||_* + l ||E||_1. [2]_
+
+    tol : float >= 0 (default 1E-7)
+        Tolerance for accuracy of matrix reconstruction of low rank and sparse
+        components.
+
+    max_iter : int >= 0 (default 1000)
+        Maximum number of iterations to perform.
+
+    Attributes
+    ----------
+
+    low_rank_ : array, (n_samples, n_features)
+        Low-rank component of the matrix decomposition.
+
+    sparse_ : array, (n_samples, n_features)
+        Sparse component of the matrix decomposition.
+
+    error_ : float
+        Error of matrix reconstruction
+
+
+    References
+    ----------
+
+    .. [1] Z. Lin, M. Chen, Y. Ma. The Augmented Lagrange Multiplier Method for
+           Exact Recovery of Corrupted Low-Rank Matrices, arXiv:1009.5055
+
+    .. [2] E. J. Candés, X. Li, Y. Ma, J. Wright. Robust principal
+           component analysis? Journal of the ACM v.58 n.11 May 2011
+
+    """
+
     def __init__(self, method='sparse',
                  mu=None,
                  l=None,
-                 max_iter=1000,
-                 tol=1E-7):
+                 tol=1E-7,
+                 max_iter=1000):
 
-        if not (method == 'sparse' or method == 'recovery'):
-            raise ValueError('method must be either `sparse` or `recovery`')
+        options = ['sparse', 'low_rank']
+
+        if method not in options:
+            raise ValueError('method must be one of %s' % options)
 
         self._method = method
         self._mu = mu
         self._l = l
         self._tol = tol
 
+        self.low_rank_ = None
+        self.sparse_ = None
+
     def transform(self, X):
-        self.A_, self.E_ = rpca(X, mu=self._mu, l=self._l, tol=self._tol)
+        """Matrix decomposition/recovery with RPCA and ALM
+
+        Parameters
+        ----------
+
+        X : array-like, shape (n_samples, n_features)
+            Matrix to decompose, where n_samples in the number of samples and n_features
+            is the number of features.
+
+        Returns
+        -------
+
+        X_new : array-like, shape (n_samples, n_features)
+            Decomposed matrix, either sparse component (method `sparse`) or
+            low-rank component (method `low_rank`)
+        """
+        self.low_rank_, self.sparse_, self.error_ = rpca(X, mu=self._mu, l=self._l, tol=self._tol)
         if self._method == 'sparse':
-            return self.E_
-        elif self._method == 'recovery':
-            return self.A_
+            return self.sparse_
+        elif self._method == 'low_rank':
+            return self.low_rank_
         else:
             raise ValueError('unknown method')
